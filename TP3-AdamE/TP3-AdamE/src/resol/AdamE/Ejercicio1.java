31ee1304cb591593b979086f8aed76d5155f2042
edu2adam <eduardo.j.adam@gmail.com>
2026-05-04 14:25:54 -0300

    Primer Commit

---
+++ TP3-AdamE/TP3-AdamE/src/resol/AdamE/Ejercicio1.java
@@ -0,0 +1,80 @@
+package resol.AdamE;  
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void ejecutar() {
+        Scanner scanner = new Scanner(System.in);
+        int [] arreglo = new int[10];
+        int total=11;
+        for (int i = 0; i < arreglo.length; i++) {
+            total=total-1;
+            System.out.println("Ingrese el numero entero (" + total +" faltan ingresar):");
+            arreglo[i] = scanner.nextInt();
+            
+        }
+
+        for(int n=0;n<10;n++){
+           primo(arreglo[n]);
+        }
+
+        int promedio = promPrimo(arreglo);
+
+        System.out.println("El promedio de los numeros primos es: " + promedio);
+
+
+    }
+
+    public static void primo(int n){
+        boolean esPrimo = true;
+        if (n <= 1) {
+            esPrimo = false;
+        } else {
+            for (int i = 2; i <=  Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    esPrimo = false;
+                    break;
+                }
+            }
+        }
+        if(esPrimo==true){
+            System.out.println(n + " es un numero primo.");
+            
+        }else{
+            System.out.println(n + " no es un numero primo.");
+        }
+        return ; 
+    }
+
+    public static boolean primosBoolean(int n){
+                boolean esPrimo = true;
+        if (n <= 1) {
+            esPrimo = false;
+        } else {
+            for (int i = 2; i <=  Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    esPrimo = false;
+                    break;
+                }
+            }
+        }
+        return esPrimo;
+    }
+
+
+    public static int promPrimo(int arreglo[]){
+        int suma=0;
+        int cantidadPrimos=0;
+        int contador=0;
+        for(int i=0;i<10;i++){
+            if(primosBoolean(arreglo[i])==true){
+                suma=suma+arreglo[i];
+                contador++;
+            }
+        }
+        int promedio = suma/contador;
+        return promedio;
+    }
+}
+
+
---
+++ TP3-AdamE/TP3-AdamE/src/resol/AdamE/Principal.java
@@ -0,0 +1,58 @@
+package resol.AdamE;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Selecione el Ejercicio que desea ejecutar:");
+        System.out.println(" 1. Ejercicio 1");
+        System.out.println(" 2. Ejercicio 2");
+        System.out.println(" 3. Ejercicio 3");
+        System.out.println(" 4. Ejercicio 4");
+        System.out.println(" 5. Ejercicio 5");
+        System.out.println(" 6. Ejercicio 6");
+        System.out.println(" 7. Ejercicio 7");
+        System.out.println(" 8. Ejercicio 8");
+        System.out.println(" 9. Ejercicio 9");
+        System.out.println("10. Ejercicio 10");
+        //int opcion = scanner.nextInt();
+        int opcion= new Scanner(System.in).nextInt();
+        switch (opcion) {
+            case 1:
+                Ejercicio1.ejecutar();
+                break;
+            /*case 2:
+                Ejercicio2.ejecutar();
+                break;
+            case 3:
+                Ejercicio3.ejecutar();
+                break;
+            case 4:
+                Ejercicio4.ejecutar();
+                break;
+            case 5:
+                Ejercicio5.ejecutar();
+                break;
+            case 6:
+                Ejercicio6.ejecutar();
+                break;
+            case 7:
+                Ejercicio7.ejecutar();
+                break;
+            case 8:
+                Ejercicio8.ejecutar();
+                break;
+            case 9:
+                Ejercicio9.ejecutar();
+                break;
+            case 10:
+                Ejercicio10.ejecutar();
+                break;*/
+        
+            default:
+                System.out.println("Opcion no valida");
+        }
+        scanner.close();
+
+    }
+}
